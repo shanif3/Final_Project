b6d8556f59c54dc048dfb7546cbcbd2c941f28de
shanif3 <shaninn123@gmail.com>
2023-10-06 01:17:55 +0300

    adding new welcome page

--- examples/ble_configuration_sample/src/main/java/com/digi/xbee/sample/android/bleconfiguration/OpenScreen.java
+++ examples/ble_configuration_sample/src/main/java/com/digi/xbee/sample/android/bleconfiguration/OpenScreen.java
@@ -1,4 +1,34 @@
 package com.digi.xbee.sample.android.bleconfiguration;
 
-public class OpenScreen {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OpenScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.open_screen);
+
+        Button buttonWith = findViewById(R.id.buttonWith);
+        buttonWith.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OpenScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonWithout = findViewById(R.id.buttonWithout);
+        buttonWithout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OpenScreen.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
