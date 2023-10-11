a9571dac67b20c49560f8c04296d85590c6bdc26
TomerZack <110763328+TomerZack@users.noreply.github.com>
2023-10-12 00:53:20 +0300

    kplkl

--- examples/ble_configuration_sample/src/main/java/com/digi/xbee/sample/android/bleconfiguration/DeviceInfo.java
+++ examples/ble_configuration_sample/src/main/java/com/digi/xbee/sample/android/bleconfiguration/DeviceInfo.java
@@ -5,52 +5,8 @@ import com.firebase.geofire.GeoLocation;
 public class DeviceInfo {
     private String hashedLocation;
 
-    public String getHashedLocation() {
-        return hashedLocation;
-    }
-
-    public void setHashedLocation(String hashedLocation) {
-        this.hashedLocation = hashedLocation;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public VehicleType getDeviceType() {
-        return deviceType;
-    }
-
-    public void setDeviceType(VehicleType deviceType) {
-        this.deviceType = deviceType;
-    }
-
-    public GeoLocation getLocation() {
-        return location;
-    }
-
-    public void setLocation(GeoLocation location) {
-        this.location = location;
-    }
-
-    public String getLastModified() {
-        return lastModified;
-    }
-
-    public void setLastModified(String lastModified) {
-        this.lastModified = lastModified;
     }
 
     private String address;
@@ -59,14 +15,6 @@ public class DeviceInfo {
     private GeoLocation location;
     private String lastModified;
     private String sessionId;
-
-    public String getSessionId() {
-        return sessionId;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
 
     public DeviceInfo(String address, String name, VehicleType deviceType, GeoLocation location, String hashedLocation, String lastModified, String sessionId) {
         this.address = address;
